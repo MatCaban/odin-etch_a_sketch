bd40d56a1cd9155800e307761ea90cb28c3a0f46
MatCaban <caban.matus@gmail.com>
2024-03-18 22:12:07 +0100

    Add mouseover event listener to boxes

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,10 @@ for(let i=0; i<256; i++){
     div.className = "box";
     mainDiv.appendChild(div);
 }
+
+const boxes = document.querySelectorAll(".box");
+boxes.forEach(box => {
+    box.addEventListener("mouseover", (e)=>{
+        e.target.classList.add("background");
+    })
+});
