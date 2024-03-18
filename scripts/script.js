fdd73d6440eb29bbd25f25278c4de8aa7e5f0eec
MatCaban <caban.matus@gmail.com>
2024-03-18 22:41:36 +0100

    Refactor code to create grid of squares based on user input

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,31 @@
 const mainDiv = document.querySelector("#gameField")
 
-for(let i=0; i<256; i++){
-    const div = document.createElement("div");
-    div.className = "box";
-    mainDiv.appendChild(div);
+// 
+const createGrid = numOfSquares => {
+    for (let i = 0; i < numOfSquares; i++) {
+        const div = document.createElement("div");
+        div.className = "box";
+        mainDiv.appendChild(div);
+    }
 }
 
-const boxes = document.querySelectorAll(".box");
-boxes.forEach(box => {
-    box.addEventListener("mouseover", (e)=>{
-        e.target.classList.add("background");
-    })
-});
+const createSquares = () => {
+    const message = prompt("Number of squares per side?");
+    console.log(message);
+    createGrid(message * message);
+
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach(box => {
+        box.addEventListener("mouseover", (e) => {
+            e.target.classList.add("background");
+        })
+    });
+
+}
 
 const button = document.querySelector("button");
-button.addEventListener("click", (e)=>{
-    const message = prompt("message");
-    console.log(message);
-})
+button.addEventListener("click", createSquares);
+
+
+
+
