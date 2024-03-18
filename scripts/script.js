787b8bae3be71259a74219a85608ce0efafb04d5
MatCaban <caban.matus@gmail.com>
2024-03-18 22:55:50 +0100

    Refactor grid creation and sizing

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,23 @@
 const mainDiv = document.querySelector("#gameField")
 
-// 
+// create grid based on number of squares
 const createGrid = numOfSquares => {
     for (let i = 0; i < numOfSquares; i++) {
         const div = document.createElement("div");
         div.className = "box";
         mainDiv.appendChild(div);
+        const squarePerRow = numOfSquares ** 0.5;
+
+        // change widht and height so squares will fit the grid
+        const boxes = document.querySelectorAll(".box");
+        boxes.forEach(box => {
+            box.style.width = `calc((960px - ${squarePerRow}px) / ${squarePerRow})`;
+            box.style.height = `calc((960px - ${squarePerRow}px) / ${squarePerRow})`;
+        })
     }
 }
-
+//ask user to input number of boxex per row 
+//put that value squaret as argument for create grid
 const createSquares = () => {
     const message = prompt("Number of squares per side?");
     console.log(message);
