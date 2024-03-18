607a148d4c6552c803ae874c1fafe394f7091708
MatCaban <caban.matus@gmail.com>
2024-03-18 21:54:58 +0100

    Update gameField size and box dimensions

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 const mainDiv = document.querySelector("#gameField")
 
-for(let i=0; i<8; i++){
+for(let i=0; i<256; i++){
     const div = document.createElement("div");
     div.className = "box";
     mainDiv.appendChild(div);
